52b475079a52d705aa2b1f872af95c699db50dc0
mahdi <mahdi.talaee1379@gmail.com>
2022-02-02 15:05:46 +0330

    Remove 2 lines from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,6 @@ buildscript {
         classpath(GradleDependencies.app_metrica)
         classpath(GradleDependencies.navigation_safe_args)
         classpath(GradleDependencies.hilt_plugin)
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("com.android.tools.build:gradle:7.1.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
