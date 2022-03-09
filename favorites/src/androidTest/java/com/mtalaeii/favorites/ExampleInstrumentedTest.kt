794a7ba2cfe2c770f0c06bc1d591336dca247244
mahdi <mahdi.talaee1379@gmail.com>
2022-03-09 17:12:55 +0330

    some bug fixed! end release

--- favorites/src/androidTest/java/com/mtalaeii/favorites/ExampleInstrumentedTest.kt
+++ favorites/src/androidTest/java/com/mtalaeii/favorites/ExampleInstrumentedTest.kt
@@ -8,19 +8,19 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    fun useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.mtalaeii.favorites.test", appContext.getPackageName());
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext;
+        assertEquals("com.mtalaeii.favorites.test", appContext.packageName);
     }
 }
